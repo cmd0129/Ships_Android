886358d3544200f4c1256ed311c278b873892be8
Przemysław Biskup <ccmmdd0129@gmail.com>
2019-05-07 13:25:13 +0200

    Final

---
+++ app/src/main/java/com/example/b5400/statki/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.b5400.statki;
+
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void klikinstrukcja (View v){
+        startActivity(new Intent(MainActivity.this, Instrukcja.class));
+    }
+}
